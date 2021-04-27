04c8254fbda9c53b72d9337d51e1d45fff533d23
rochaadm23 <ribeiro.ribeiro821@gmail.com>
2021-04-27 15:58:51 -0300

    Classe de notação ponto

---
+++ projetojava2/src/fundamentos/NotacaoPonto.java
@@ -0,0 +1,18 @@
+package fundamentos;
+
+public class NotacaoPonto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double a = 2.3;
+		String s = "Bom dia X";
+		s = s.concat("!!!");
+		System.out.println(s.length());
+		System.out.println(s.toUpperCase());
+		System.out.println(s.toLowerCase());
+		System.out.println(s.replace("X", "Jovem"));
+		
+	}
+
+}
