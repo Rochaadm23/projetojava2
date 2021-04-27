7fa93f1a845e2a8f2bd62d3263e7c15ba1b90300
rochaadm23 <ribeiro.ribeiro821@gmail.com>
2021-04-27 13:44:10 -0300

    Inferência de tipos

---
+++ projetojava2/src/fundamentos/Inferencia.java
@@ -0,0 +1,16 @@
+package fundamentos;
+
+public class Inferencia {
+	public static void main(String[] args) {
+		
+		double a = 4.5; //Variável declarada e inicializada.
+		
+		System.out.println(a);
+		
+		var b = 6.5; // com o var você deve declarar a variável e inicia-la na mesma linha.
+		System.out.println(b);
+		
+		var c = "texto";
+		System.out.println(c);
+	}
+}
